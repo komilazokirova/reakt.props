1bb490dc4debf84118eda273bdbd85bda394e440
Komila Zokirova <zokirovakomila500@gmail.com>
2026-06-17 21:34:49 +0500

    React project added

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,29 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return(
+  return (
     <div>
-      <Header title="Komila VIbe"/>
-      <Navbar 
-      name="Komila"
-      age={22}
-      job="Frontend Developer"
-      
+      <Header title="Komila VIbe" />
+      <Navbar
+        name="Komila"
+        age={22}
+        job="Frontend Developer"
       />
       <main>
         <h2>Asosiy saxifa</h2>
+        <p>Bu sahifada React asoslarini - komponentlar, props va state'ni o'rganamiz.</p>
+        <section>
+          <h3>Mening loyihalarim</h3>
+          <ul>
+            <li>Portfolio sayti</li>
+            <li>To-do ilovasi</li>
+            <li>Weather app</li>
+          </ul>
+        </section>
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
